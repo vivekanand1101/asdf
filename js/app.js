78c76ee868198eb5477a0bbb033db750e1552f7b
Vivek Anand <vivekanand1101@gmail.com>
2018-02-19 23:47:26 +0530

    Add jquery and startup code
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
  * Create a list that holds all of your cards
  */
 
+var cards = $(".card");
 
 /*
  * Display the cards on the page
@@ -25,6 +26,8 @@ function shuffle(array) {
     return array;
 }
 
+shuffle(cards);
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +39,16 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+var open_cards = [];
+
+$(".card").on('click', function(e) {
+  // Open / Close the symbol display
+  $(this).toggleClass("open show");
+  // Add to the list of open cards
+  open_cards.push($(this));
+  if (open_cards.length >= 1) {
+  }
+});
+
